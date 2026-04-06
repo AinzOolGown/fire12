0d4084abbfce61bc8660aec197a85939217b6021
Bruno Rico <ricobruno58@gmail.com>
2026-04-06 17:58:51 -0400

    generic additem added to commands

--- lib/firestore_manager.dart
+++ lib/firestore_manager.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'item.dart';
 
 class FirestoreManager {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
@@ -7,4 +8,13 @@ class FirestoreManager {
   final String collectionPath = "posts";
 
   CollectionReference get _collection => _db.collection(collectionPath);
+
+  Future<void> addItem(Map<String, dynamic> data) async {
+    try {
+      await _collection.add(data);
+    } catch (e) {
+      throw Exception("Failed to add item: $e");
+    }
+  }
 }
+
